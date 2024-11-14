e7b4e104eb5abb13d2ca5c79c05d13f210a1c98b
Ivan Ukhov <ivan.ukhov@gmail.com>
2024-11-14 11:44:24 +0100

    Remove serde

--- Makefile
+++ Makefile
@@ -1,23 +1,13 @@
-features := serde serde_json
-
 .PHONY: all
 all: check test
 
 .PHONY: check
-check: $(addprefix check-,$(features))
+check:
 	cargo clippy --no-default-features -- -D warnings
 	cargo clippy --all-features -- -D warnings
 	cargo fmt --all -- --check
 
-.PHONY: $(addprefix check-,$(features))
-$(addprefix check-,$(features)): check-%:
-	cargo clippy --features $* -- -D warnings
-
 .PHONY: test
-test: $(addprefix test-,$(features))
+test:
 	cargo test --no-default-features
 	cargo test --all-features
-
-.PHONY: $(addprefix test-,$(features))
-$(addprefix test-,$(features)): test-%:
-	cargo build --features $*
--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,12 @@
 //! Means of configuration.
 
-#[cfg(feature = "serde")]
-#[doc(hidden)]
-pub extern crate serde;
-
-#[cfg(feature = "serde_json")]
-#[doc(hidden)]
-pub extern crate serde_json;
-
 /// Define a configuration.
 ///
 /// ```
 /// use std::net::SocketAddr;
 /// use std::path::{Path, PathBuf};
 ///
-/// configuration::config! {
+/// configuration::define! {
 ///     /// A config.
 ///     Config {
 ///         /// The foo config.
@@ -27,13 +19,11 @@ pub extern crate serde_json;
 ///                 /// The buz.
 ///                 buz: PathBuf = [
 ///                     CONFIG_FOO_BUZ,
-///                     "config_foo_buz",
 ///                     config_foo_buz() { "buz".into() }
 ///                 ],
 ///                 /// The qux.
 ///                 qux: usize = [
 ///                     CONFIG_FOO_QUX,
-///                     "config_foo_qux",
 ///                     config_foo_qux() { 42 }
 ///                 ],
 ///             },
@@ -53,7 +43,6 @@ pub extern crate serde_json;
 ///                 /// The qux.
 ///                 qux: SocketAddr = [
 ///                     CONFIG_BUZ_QUX,
-///                     "config_buz_qux",
 ///                     config_buz_qux() { "0.0.0.0:80".parse().unwrap() }
 ///                 ],
 ///             },
@@ -75,7 +64,7 @@ pub extern crate serde_json;
 /// assert_eq!(config.qux, "qux");
 /// ```
 #[macro_export]
-macro_rules! config {
+macro_rules! define {
     (
         $(#[$struct_attribute:meta])*
         $struct:ident {
@@ -91,11 +80,8 @@ macro_rules! config {
                     }
                 )?
                 $(= [
+                    $default_variable:ident $(,)?
                     $(
-                        $default_variable:ident $(,)?
-                    )?
-                    $(
-                        $default_literal:literal,
                         $default_function:ident ()
                         $default_block:block
                     )?
@@ -105,32 +91,22 @@ macro_rules! config {
     ) => (
         $(#[$struct_attribute])*
         #[derive(Clone, Debug)]
-        #[cfg_attr(feature = "serde", derive($crate::serde::Deserialize, $crate::serde::Serialize))]
         pub struct $struct {
             $(
                 $(#[$field_attribute])*
-                $($(#[cfg_attr(feature = "serde", serde(default = $default_literal))])?)?
                 pub $field: $type,
             )+
         }
 
         impl $struct {
-            /// Create an instance from a JSON file.
-            #[cfg(feature = "serde_json")]
-            #[inline]
-            pub fn new<T: AsRef<std::path::Path>>(path: T) -> std::io::Result<Self> {
-                let file = std::fs::File::open(path)?;
-                Ok($crate::serde_json::from_reader(file)?)
-            }
-
             /// Create an instance from the environment.
             #[allow(clippy::should_implement_trait)]
             pub fn default() -> std::io::Result<Self> {
                 Ok(Self {
                     $(
-                        $field: $crate::config!(
+                        $field: $crate::define!(
                             @default
-                            $type [$($inner)?] $($($default_variable)? $($default_function())?)?
+                            $type [$($inner)?] $($default_variable $($default_function())?)?
                         ),
                     )+
                 })
@@ -148,7 +124,7 @@ macro_rules! config {
 
         $(
             $(
-                $crate::config! {
+                $crate::define! {
                     $(#[$type_attribute])*
                     $inner {
                         $($nested)+
@@ -165,19 +141,13 @@ macro_rules! config {
     );
     (@default $type:ty [$($inner:ident)?] $variable:ident) => (
         match std::env::var(stringify!($variable)) {
-            Ok(value) => match std::str::FromStr::from_str(&value) {
-                Ok(value) => value,
-                Err(error) => return Err(std::io::Error::other(error)),
-            },
+            Ok(value) => std::str::FromStr::from_str(&value).map_err(|error| std::io::Error::other(error))?,
             _ => <$type>::default(),
         }
     );
     (@default $type:ty [$($inner:ident)?] $variable:ident $function:ident ()) => (
         match std::env::var(stringify!($variable)) {
-            Ok(value) => match std::str::FromStr::from_str(&value) {
-                Ok(value) => value,
-                Err(error) => return Err(std::io::Error::other(error)),
-            },
+            Ok(value) => std::str::FromStr::from_str(&value).map_err(|error| std::io::Error::other(error))?,
             _ => $function(),
         }
     );
